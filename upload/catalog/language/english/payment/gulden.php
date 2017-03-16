387aa9a3335b5007777869a53a59bc35c3a09b6c
Perry Peeters <perry@verspa.nl>
2017-03-16 11:53:15 +0100

    Update gulden.php
    corrected

--- upload/catalog/language/english/payment/gulden.php
+++ upload/catalog/language/english/payment/gulden.php
@@ -3,4 +3,4 @@
 $_['text_title']           = 'Gulden';
 $_['text_instruction']     = 'Payment Instructions';
 $_['text_description']     = 'Please transfer %s to the following Gulden Address:';
-$_['text_bitcoin_address'] = 'Payment Address: %s';
+$_['text_gulden_address']  = 'Payment Address: %s';
