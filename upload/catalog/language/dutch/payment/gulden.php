9b0a1d67ccc20857dbeab0e83619864f8f9fee21
Perry Peeters <perry@verspa.nl>
2017-03-16 11:52:25 +0100

    Create gulden.php

---
+++ upload/catalog/language/dutch/payment/gulden.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title']           = 'Gulden';
+$_['text_instruction']     = 'Betalingsinstructies';
+$_['text_description']     = 'Maak aub %s over naar het volgende Gulden Adres:';
+$_['text_gulden_address']  = 'Betaaladres: %s';
